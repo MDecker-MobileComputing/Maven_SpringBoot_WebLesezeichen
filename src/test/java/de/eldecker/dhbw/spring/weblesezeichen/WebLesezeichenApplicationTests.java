cd4fa9a89ae6643178e497c08080cabe76b26fe0
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-05-28 09:31:38 +0200

    Projekt neu angelegt. https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.3.0&packaging=jar&jvmVersion=17&groupId=de.eldecker.dhbw.spring&artifactId=weblesezeichen&name=Web-Lesezeichen&description=Baumstruktur%20mit%20JPA&packageName=de.eldecker.dhbw.spring.weblesezeichen&dependencies=data-jpa,h2,thymeleaf

---
+++ src/test/java/de/eldecker/dhbw/spring/weblesezeichen/WebLesezeichenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.weblesezeichen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLesezeichenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
